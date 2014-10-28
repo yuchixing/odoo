175b99f821a90467f8a9643d42074a8c67c54f85
yuchixing <void.Yu@hotmail.com>
2014-10-29 00:25:23 +0800

    update extend_hr

---
+++ addons/exend_hr/__openerp__.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+
+{
+    'name':u'考勤模块',
+    'version':'1.0',
+    'category':'tools',
+    'website':'',
+    'author':'voidyu',
+    'depends':['hr','report'],
+    'data':[],
+    'demo':[],
+    'auto_install':True,
+    'installable':True,
+    'qweb':[],
+
+}
