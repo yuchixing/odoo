88d9849da0a5bbf364afd5ba39c669ff6a618b3a
yuchixing <void.Yu@hotmail.com>
2014-11-08 15:07:28 +0800

    update

--- addons/weixin/controllers/main.py
+++ addons/weixin/controllers/main.py
@@ -5,12 +5,19 @@ from openerp.addons.web.http import request
 from openerp.tools.translate import _
 
 import json
-
+import hashlib
+#hashlib.sha1('This is a sha1 test!').hexdigest()
 
 class Weixin(http.Controller):
     @http.route(['/weixin'], type='http',auth="public", website=True)
-    def weixin_reload(self):
+    def weixin_reload(self,signature,timestamp,nonce,echostr):
         print '*'*10
+        param_list=[timestamp,nonce,echostr]
+        param_list.sort()
+        param=[str(a) for a in param_list]
+        signature_bak=''.join(param)
+        if signature==hashlib.sha1(signature_bak).hexdigest:
+            print 'True'
         return {'a':'a'}
 
 
